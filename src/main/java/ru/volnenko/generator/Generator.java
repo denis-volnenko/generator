931f7fe82ddbf7d30260b449c5fda3a069a3c601
Denis Volnenko <denis@volnenko.ru>
2020-05-24 11:11:08 +0300

    Добавлена генерация даты.

--- src/main/java/ru/volnenko/generator/Generator.java
+++ src/main/java/ru/volnenko/generator/Generator.java
@@ -1,11 +1,20 @@
 package ru.volnenko.generator;
 
+import java.util.Date;
 import java.util.UUID;
 
 public final class Generator {
 
     public static String uuid() {
         return UUID.randomUUID().toString();
+    }
+
+    public static Long timestamp() {
+        return System.currentTimeMillis();
+    }
+
+    public static Date now() {
+        return new Date();
     }
 
 }
