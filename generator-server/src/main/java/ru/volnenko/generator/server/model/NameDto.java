9fb689fc5433511063545480292756d06e826bec
Denis Volnenko <denis@volnenko.ru>
2025-04-14 10:40:43 +0400

    Project updated.

--- generator-server/src/main/java/ru/volnenko/generator/server/model/NameDto.java
+++ generator-server/src/main/java/ru/volnenko/generator/server/model/NameDto.java
@@ -1,7 +1,16 @@
 package ru.volnenko.generator.server.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class NameDto {
 
+    @NonNull
     private String name = "";
 
 }
