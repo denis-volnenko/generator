ef3b45298796f51143a93c33f6d9ce933c2ccda6
Denis Volnenko <denis@volnenko.ru>
2025-04-14 10:51:15 +0400

    Generator Server updated.

---
+++ generator-server/src/main/java/ru/volnenko/generator/server/model/FullNameDto.java
@@ -0,0 +1,22 @@
+package ru.volnenko.generator.server.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public final class FullNameDto {
+
+    @NonNull
+    private String firstName = "";
+
+    @NonNull
+    private String lastName = "";
+
+    @NonNull
+    private String middleName = "";
+
+}
